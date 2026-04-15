0b3b20928aaf99679673ab91d9408e4d8a133021
Mark Ramos <mark.ramos@disney.com>
2026-04-14 22:16:14 -0700

    fix: add babel-plugin-transform-typescript-metadata for typeorm decorator support
    TypeORM requires decorator metadata (design:type, design:paramtypes,
    design:returntype) to resolve entity relationships. Babel's
    @babel/plugin-proposal-decorators with legacy mode handles decorator
    syntax but does NOT emit this metadata.
    
    babel-plugin-transform-typescript-metadata bridges the gap, emitting
    the same reflect-metadata that tsc produces with emitDecoratorMetadata.
    
    Combined with keep_classnames (PR #35), this fully fixes #32.
    
    The plugin must be listed BEFORE @babel/plugin-proposal-decorators in
    the plugin chain so it can capture metadata before decorators transform.
    
    Closes #32

--- packages/server/scripts/webpack.main.config.js
+++ packages/server/scripts/webpack.main.config.js
@@ -32,6 +32,7 @@ module.exports = merge(baseConfig, {
                         "@babel/preset-typescript"
                     ],
                     plugins: [
+                        "babel-plugin-transform-typescript-metadata",
                         ["@babel/plugin-proposal-decorators", { legacy: true }],
                         [
                             "@babel/plugin-transform-class-properties",
